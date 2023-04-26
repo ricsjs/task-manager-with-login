97dbf43c690180f01e9291fa3d99e28ea77f263d
Ricardo Alencar <ricfilho0007@gmail.com>
2023-04-26 15:45:11 -0300

    Criação de rota privada

---
+++ src/routes/Private.js
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+
+import { auth } from "../firebaseConnection";
+import { onAuthStateChanged } from "firebase/auth";
+
+import { Navigate } from "react-router-dom";
+
+function Private({children}){
+    const [loading, setLoading] = useState(true);
+    const [signed, setSigned] = useState(false);
+
+    useEffect(() => {
+        async function checkLogin(){
+            const unsub = onAuthStateChanged(auth, (user) => {
+                //se logado
+                if(user){
+                    const userData = {
+                        uid: user.uid,
+                        email: user.email,
+                    }
+
+                    localStorage.setItem("@detailUser", JSON.stringify(userData))
+
+                    setLoading(false);
+                    setSigned(true);
+                }else{
+                    //n ta logado
+                    setLoading(false);
+                    setSigned(false);
+                }
+            })
+        }
+
+        checkLogin();
+
+    }, [])
+
+
+    if(loading) {
+        return(
+            <div>
+
+            </div>
+        )
+    }
+
+    if(!signed) {
+        return <Navigate to='/'/>
+    }
+    
+    return children;
+
+
+}
+
+export default Private;

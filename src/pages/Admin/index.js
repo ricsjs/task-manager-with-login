b77b130e74059ed85a3b3aef6dd1d45b3d45e4e5
Ricardo Alencar <ricfilho0007@gmail.com>
2023-04-27 09:38:57 -0300

    adicionando função de adicionar tarefas

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,18 +1,44 @@
 import './Admin.css'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-import { auth } from '../../firebaseConnection'
+import { auth, db } from '../../firebaseConnection'
 import { signOut } from 'firebase/auth';
 import { addDoc, collection } from 'firebase/firestore'
 
 function Admin(){
 
     const [tarefaInput, setTarefaInput] = useState();
+    const [user, setUser] = useState({})
 
-    function handleRegister(e){
+    useEffect(() => {
+        async function loadTarefas(){
+            const userDetail = localStorage.getItem("@detailUser")
+            setUser(JSON.parse(userDetail))
+        }
+
+        loadTarefas();
+    }, [])
+
+    async function handleRegister(e){
         e.preventDefault();
 
-        
+        if(tarefaInput === ''){
+            alert("Digite sua tarefa...")
+            return;
+        }
+
+        await addDoc(collection(db, 'tarefas'), {
+            tarefa: tarefaInput,
+            created: new Date(),
+            userUild: user?.uid
+        })
+        .then(() => {
+            console.log("Tarefa registrada")
+            setTarefaInput('')
+        })
+        .catch((error) => {
+            console.log(error)
+        })
     }
 
     async function handleLogout(){
